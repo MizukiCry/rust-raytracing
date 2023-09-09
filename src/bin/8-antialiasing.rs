bd8e99b4e83f783544f53ffb19180eeff7508860
MizukiCry <YuukaC@outlook.com>
2023-09-09 21:11:02 +0800

    Saves chapter 8 codes
    Signed-off-by: MizukiCry <YuukaC@outlook.com>

---
+++ src/bin/8-antialiasing.rs
@@ -0,0 +1,20 @@
+// // cargo run --bin 8-antialiasing | out-file output.ppm -encoding ascii
+// use rust_raytracing::utils::*;
+// use std::rc::Rc;
+
+// fn main() {
+//     let mut camera = Camera::new(16.0 / 9.0, 20, 500);
+
+//     let mut world = HittableList::default();
+//     world.add(Rc::new(Box::new(Sphere::new(
+//         Vec3::new(0.0, 0.0, -1.0),
+//         0.5,
+//     ))));
+//     world.add(Rc::new(Box::new(Sphere::new(
+//         Vec3::new(0.0, -100.5, -1.0),
+//         100.0,
+//     ))));
+
+//     camera.render(&world);
+// }
+fn main() {}
